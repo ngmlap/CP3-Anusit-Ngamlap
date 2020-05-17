0cbebb0a7f13db0f9918bfa6d80eece365a81d6e
ngamlap <ngamlap.a@gmail.com>
2020-05-17 15:33:15 +0700

    Exercise5_1_Anusit_N
    variable

---
+++ assignments/Exercise5_1_Anusit_N.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number= "))
+num2=int(input("enter second number= "))
+plus=num1+num2
+minus=num1-num2
+multiply=num1*num2
+divide=num1/num2
+print(num1,"+",num2,"=",plus)
+print(num1,"-",num2,"=",minus)
+print(num1,"*",num2,"=",multiply)
+print(num1,"/",num2,"=",divide)

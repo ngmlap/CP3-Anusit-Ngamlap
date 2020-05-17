0933f846b4b1ecfb69381114a9a5eeaf5484a78d
ngamlap <ngamlap.a@gmail.com>
2020-05-17 11:06:26 +0700

    exercise4

---
+++ assignments/Exercise4_Anusit_N.py
@@ -0,0 +1,10 @@
+score_english = 80.05
+score_business = 84.75
+score_computer_systems = 90.80
+score_programming = 90.00
+text1 = "--- Your Score ---"
+print(text1)
+print("Foundation English :",score_english)
+print("General Business :",score_business)
+print("Introduction to Computer Systems :",score_computer_systems)
+print("Computer Programming :",score_programming)
